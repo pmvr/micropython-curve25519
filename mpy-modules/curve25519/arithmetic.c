1390b505ce93271fcb12e2c6c6749522869f9885
pmvr <matthias.voegeler@hshl.de>
2020-06-09 19:31:53 +0200

    combining mul+add in one call

--- mpy-modules/curve25519/arithmetic.c
+++ mpy-modules/curve25519/arithmetic.c
@@ -70,93 +70,159 @@ void sub_zxy_mod_p(uint32_t *z, uint32_t *x, uint32_t *y, uint32_t *p) {
 }
 
 
-void mul_zxy(uint32_t *z, uint32_t *x, uint32_t y) {
+void mul_add_zxy(uint32_t *z, uint32_t *x, uint32_t y) {
+    // z += x*y
+    // Note, UMAAL is not available
     __asm__ volatile (
     // 0
     "LDMIA  %0!, {r3}\n"
     "UMULL  r5, r6, r3, %1\n"
+    "LDMIA  %2, {r3}\n"
+    "ADDS   r5, r5, r3\n"
     "STMIA  %2!, {r5}\n"
     // 1
     "LDMIA  %0!, {r3}\n"
     "MOV    r5, 0\n"
     "UMLAL  r6, r5, r3, %1\n"
+    "LDMIA  %2, {r3}\n"
+    "ADCS   r6, r6, r3\n"
     "STMIA  %2!, {r6}\n"
     // 2
     "LDMIA  %0!, {r3}\n"
     "MOV    r6, 0\n"
     "UMLAL  r5, r6, r3, %1\n"
+    "LDMIA  %2, {r3}\n"
+    "ADCS   r5, r5, r3\n"
     "STMIA  %2!, {r5}\n"
     // 3
     "LDMIA  %0!, {r3}\n"
     "MOV    r5, 0\n"
     "UMLAL  r6, r5, r3, %1\n"
+    "LDMIA  %2, {r3}\n"
+    "ADCS   r6, r6, r3\n"
     "STMIA  %2!, {r6}\n"
     // 4
     "LDMIA  %0!, {r3}\n"
     "MOV    r6, 0\n"
     "UMLAL  r5, r6, r3, %1\n"
+    "LDMIA  %2, {r3}\n"
+    "ADCS   r5, r5, r3\n"
     "STMIA  %2!, {r5}\n"
     // 5
     "LDMIA  %0!, {r3}\n"
     "MOV    r5, 0\n"
     "UMLAL  r6, r5, r3, %1\n"
+    "LDMIA  %2, {r3}\n"
+    "ADCS   r6, r6, r3\n"
     "STMIA  %2!, {r6}\n"
     // 6
     "LDMIA  %0!, {r3}\n"
     "MOV    r6, 0\n"
     "UMLAL  r5, r6, r3, %1\n"
+    "LDMIA  %2, {r3}\n"
+    "ADCS   r5, r5, r3\n"
     "STMIA  %2!, {r5}\n"
     // 7
     "LDMIA  %0!, {r3}\n"
     "MOV    r5, 0\n"
     "UMLAL  r6, r5, r3, %1\n"
+    "LDMIA  %2, {r3}\n"
+    "ADCS   r6, r6, r3\n"
     "STMIA  %2!, {r6}\n"
+    "LDMIA  %2, {r3}\n"
+    "ADCS   r5, r5, r3\n"
     "STMIA  %2, {r5}\n"
     : : "r" (x), "r" (y), "r" (z) : "r3", "r5", "r6"
     );
 }
-    
 
-void mul_zx0y0(uint32_t *z, uint32_t x, uint32_t y) {
+
+void mul_add_zx0y0(uint32_t *z, uint32_t x, uint32_t y) {
+    // z += x0 * y0
     __asm__ volatile (
     // 0
     "UMULL  r5, r6, %0, %1\n"
-    "STMIA  %2!, {r5}\n"
-    "STMIA  %2!, {r6}\n"
+    "LDMIA  %2, {r3}\n"
+    "ADDS   r3, r3, r5\n"
+    "STMIA  %2!, {r3}\n"
+    "LDMIA  %2, {r3}\n"
+    "ADCS   r3, r3, r6\n"
+    "STMIA  %2!, {r3}\n"
     "MOV    r5, 0\n"
-    "MOV    r6, 0\n"
-    "STMIA  %2!, {r5, r6}\n"
-    "STMIA  %2!, {r5, r6}\n"
-    "STMIA  %2!, {r5, r6}\n"
-    "STMIA  %2, {r5}\n"
-    : : "r" (x), "r" (y), "r" (z) : "r5", "r6"
+    "LDMIA  %2, {r3}\n"
+    "ADCS   r3, r3, r5\n"
+    "STMIA  %2!, {r3}\n"
+    "LDMIA  %2, {r3}\n"
+    "ADCS   r3, r3, r5\n"
+    "STMIA  %2!, {r3}\n"
+    "LDMIA  %2, {r3}\n"
+    "ADCS   r3, r3, r5\n"
+    "STMIA  %2!, {r3}\n"
+    "LDMIA  %2, {r3}\n"
+    "ADCS   r3, r3, r5\n"
+    "STMIA  %2!, {r3}\n"
+    "LDMIA  %2, {r3}\n"
+    "ADCS   r3, r3, r5\n"
+    "STMIA  %2!, {r3}\n"
+    "LDMIA  %2, {r3}\n"
+    "ADCS   r3, r3, r5\n"
+    "STMIA  %2!, {r3}\n"
+    "LDMIA  %2, {r3}\n"
+    "ADCS   r3, r3, r5\n"
+    "STMIA  %2, {r3}\n"
+    //"SUBS   %2, 32\n"  //*
+    : : "r" (x), "r" (y), "r" (z) : "r3", "r5", "r6"
     );
 }
 
 
-void pu(uint32_t *t, uint32_t u) {
-    // computes (2^255 -19) * u
+void pu_add_shift(uint32_t *t, uint32_t u) {
+    // computes t = (t + (2^255 -19) * u) >> 32
     __asm__ volatile (
     "MOV    r3, 19\n"
-    "UMULL  r5, r6, r3, %1\n"
+    "UMULL  r5, r6, r3, %1\n"    
     "MOV    r2, 0\n"
     "LSRS   %1, %1, 1\n"
     "RRXS   r2, r2\n"
-    "MOV    r3, 0\n"
-    "SUBS   r4, r3, r5\n"
-    "STMIA  %0!, {r4}\n"
-    "SBCS   r4, r3, r6\n"
-    "STMIA  %0!, {r4}\n"
-    "SBCS   r4, r3, r3\n"
-    "STMIA  %0!, {r4}\n"
-    "STMIA  %0!, {r4}\n"
-    "STMIA  %0!, {r4}\n"
-    "STMIA  %0!, {r4}\n"
-    "STMIA  %0!, {r4}\n"
-    "SBCS   r4, r2, r3\n"
-    "STMIA  %0!, {r4}\n"
-    "SBCS   r4, %1, r3\n"
-    "STMIA  %0, {r4}\n"
+    // t[8]:t[7] + (u << 255)
+    "ADDS   %0, %0, 28\n"
+    "LDMIA  %0, {r3}\n"
+    "ADDS   r3, r3, r2\n"
+    "STMIA  %0!, {r3}\n"
+    "LDMIA  %0, {r3}\n"
+    "ADCS   r3, r3, %1\n"
+    "STMIA  %0, {r3}\n"
+    "SUBS   %0, %0, 32\n"
+    // t - 19*u
+    "MOV    r2, %0\n"
+    "LDMIA  %0!, {r4}\n"
+    "SUBS   r4, r4, r5\n"
+    "LDMIA  %0!, {r4}\n"
+    "SBCS   r4, r4, r6\n"
+    "STMIA  r2!, {r4}\n"
+    "MOV    r5, 0\n"
+    "LDMIA  %0!, {r4}\n"
+    "SBCS   r4, r4, r5\n"
+    "STMIA  r2!, {r4}\n"
+    "LDMIA  %0!, {r4}\n"
+    "SBCS   r4, r4, r5\n"
+    "STMIA  r2!, {r4}\n"
+    "LDMIA  %0!, {r4}\n"
+    "SBCS   r4, r4, r5\n"
+    "STMIA  r2!, {r4}\n"
+    "LDMIA  %0!, {r4}\n"
+    "SBCS   r4, r4, r5\n"
+    "STMIA  r2!, {r4}\n"
+    "LDMIA  %0!, {r4}\n"
+    "SBCS   r4, r4, r5\n"
+    "STMIA  r2!, {r4}\n"
+    "LDMIA  %0!, {r4}\n"
+    "SBCS   r4, r4, r5\n"
+    "STMIA  r2!, {r4}\n"
+    "LDMIA  %0, {r4}\n"
+    "SBCS   r4, r4, r5\n"
+    "STMIA  r2!, {r4}\n"
+    "STMIA  r2, {r5}\n"  // ms-word = 0
     //"SUBS   %0, %0, 32\n"
     : : "r" (t), "r" (u) : "r2", "r3", "r4", "r5", "r6"
     );
@@ -173,13 +239,8 @@ void mont_mul_zxy_mod_p(uint32_t *z, uint32_t *x, uint32_t *y, uint32_t *p) {
     for (int i=0; i<9; i++) a[i] = 0;
     for (int i=0; i<8; i++) {
         u = (a[0] + x[i] * y[0]) * 678152731;
-        mul_zxy(tmp, y, x[i]);
-        a[8] += tmp[8] + add_zxy(a, a, tmp);
-        // mul_zxy(tmp, p, u);
-        pu(tmp, u);
-        a[8] += tmp[8] + add_zxy(a, a, tmp);  // A <- (A + xi y + u m) / b
-        for (int j=0; j<8; j++) a[j] = a[j+1];
-        a[8] = 0;
+        mul_add_zxy(a, y, x[i]);  // A <- A + xi y 
+        pu_add_shift(a, u);  // A <- (A + u m) / b
     }
     uint32_t c = sub_zxy(tmp, a, p);  // carry not set if negative
     for (int i=0; i<8; i++) z[i] = ret[c][i];
@@ -196,13 +257,8 @@ void mont_mul_zxy0_mod_p(uint32_t *z, uint32_t *x, uint32_t y, uint32_t *p) {
     for (int i=0; i<9; i++) a[i] = 0;
     for (int i=0; i<8; i++) {
         u = (a[0] + x[i] * y) * 678152731;
-        mul_zx0y0(tmp, x[i], y);
-        a[8] += tmp[8] + add_zxy(a, a, tmp);
-        // mul_zxy(tmp, p, u);
-        pu(tmp, u);
-        a[8] += tmp[8] + add_zxy(a, a, tmp);  // A <- (A + xi y + u m) / b
-        for (int j=0; j<8; j++) a[j] = a[j+1];
-        a[8] = 0;
+        mul_add_zx0y0(a, y, x[i]);  // A <- A + xi y
+        pu_add_shift(a, u);  // A <- (A + u m) / b
     }
     uint32_t c = sub_zxy(tmp, a, p);  // carry not set if negative
     for (int i=0; i<8; i++) z[i] = ret[c][i];
